be9ac3000f7944a6934a7992bc61eb0c09ecc8fb
yourdilemma <k.iankovskaia@gmail.com>
2021-03-16 23:24:37 -0400

    andrew broke kristina's app

--- routes/workoutRouter.js
+++ routes/workoutRouter.js
@@ -3,53 +3,34 @@ const db = require("../models");
 
 // Get all workouts
 Router.get("/workouts", (req, res) => {
-  db.Workout.find({}, (err, data) => {
-    if (err) {
-      throw err;
-    } else {
-      res.json(data);
-    }
-  });
+  db.Workout.find({})
+    .then((response) => res.json(response))
+    .catch((err) => res.json(err));
 });
 
 // Get all workouts
 Router.get("/workouts/range", (req, res) => {
-  db.Workout.find({}, (err, data) => {
-    if (err) {
-      throw err;
-    } else {
-      res.json(data);
-    }
-  });
+  db.Workout.find({})
+  .sort({day: -1})
+  .limit(7)
+  .then(response => res.json(response))
+  .catch(err => res.json(err));
 });
 
 // POST a new workout
 Router.post("/workouts", (req, res) => {
-  db.Workout.create(req.body, (err, data) => {
-    if (err) {
-      throw err;
-    } else {
-      res.json(data);
-    }
-  });
+  db.Workout.create(req.body)
+  .then(response => res.json(response))
+  .catch(err => res.json(err))
 });
 
 Router.put("/workouts/:_id", function (req, res) {
   db.Workout.findByIdAndUpdate(
     req.params._id,
     { $push: { exercises: req.body } },
-    { new: true, upsert: true, safe: true },
-    (err, data) => {
-      if (data) {
-        console.log(data);
-        console.log(req.params._id);
-        console.log(req.body);
-        res.status(200).json(data);
-      } else {
-        res.json(err);
-      }
-    }
-  );
+    { new: true, upsert: true, safe: true })
+    .then(response => res.json(response))
+    .catch(err => res.json(err))
 });
 
 module.exports = Router;
