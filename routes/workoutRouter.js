7c2f42774799fb8ca92df6f4ba946d611263e43a
yourdilemma <k.iankovskaia@gmail.com>
2021-03-17 00:00:26 -0400

    more andrew refactor

--- routes/workoutRouter.js
+++ routes/workoutRouter.js
@@ -2,14 +2,14 @@ const Router = require("express").Router();
 const db = require("../models");
 
 // Get all workouts
-Router.get("/workouts", (req, res) => {
+Router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
     .then((response) => res.json(response))
     .catch((err) => res.json(err));
 });
 
 // Get all workouts
-Router.get("/workouts/range", (req, res) => {
+Router.get("/api/workouts/range", (req, res) => {
   db.Workout.find({})
   .sort({day: -1})
   .limit(7)
@@ -18,13 +18,13 @@ Router.get("/workouts/range", (req, res) => {
 });
 
 // POST a new workout
-Router.post("/workouts", (req, res) => {
+Router.post("/api/workouts", (req, res) => {
   db.Workout.create(req.body)
   .then(response => res.json(response))
   .catch(err => res.json(err))
 });
 
-Router.put("/workouts/:id", function (req, res) {
+Router.put("/api/workouts/:id", function (req, res) {
   db.Workout.findByIdAndUpdate(
     req.params.id,
     { $push: { exercises: req.body } })
